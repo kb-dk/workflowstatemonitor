3c8e88de4513b18321081d11801a50d1fff9164e
Kåre Fiedler Christiansen <kfc@statsbiblioteket.dk>
2012-05-09 16:13:16 +0200

    Added an interface for states

---
+++ src/main/java/dk/statsbiblioteket/infra/workflowstatemonitor/StateManager.java
@@ -0,0 +1,11 @@
+package dk.statsbiblioteket.infra.workflowstatemonitor;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: kfc
+ * Date: 5/9/12
+ * Time: 3:56 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public interface StateManager {
+}
