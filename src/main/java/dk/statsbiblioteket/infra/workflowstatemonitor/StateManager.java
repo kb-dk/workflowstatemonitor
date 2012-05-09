8293971763633efac4f7eaf2b9b8c094fc1027db
Kåre Fiedler Christiansen <kfc@statsbiblioteket.dk>
2012-05-09 16:13:57 +0200

    Added an interface for states

--- src/main/java/dk/statsbiblioteket/infra/workflowstatemonitor/StateManager.java
+++ src/main/java/dk/statsbiblioteket/infra/workflowstatemonitor/StateManager.java
@@ -1,11 +1,51 @@
 package dk.statsbiblioteket.infra.workflowstatemonitor;
 
-/**
- * Created by IntelliJ IDEA.
- * User: kfc
- * Date: 5/9/12
- * Time: 3:56 PM
- * To change this template use File | Settings | File Templates.
- */
+import java.util.List;
+
+/** Handling a set of named states of named components for named entities. */
 public interface StateManager {
+    /**
+     * Add a state for an entity. Will add the state to the list of registered
+     * states for that entity, and timestamp it.
+     *
+     * @param entity The name of the entity.
+     * @param state The state.
+     */
+    public void addState(String entity, State state);
+
+    /**
+     * List all entities with registered states.
+     *
+     * @return A list of all entites.
+     */
+    public List<String> listEntities();
+
+    /**
+     * List ALL registered states in system.
+     *
+     * @return A list of all registered states.
+     */
+    public List<State> listStates();
+
+    /**
+     * List all registered states for the given entities.
+     *
+     * @param entity What to list states for
+     * @return A list of all registered states.
+     */
+    public List<State> listStates(String... entity);
+
+    /**
+     * List subset of registered states given by parameters.
+     *
+     * @param onlyLast If true, only list the newest registered state for each
+     * entity.
+     * @param includes If not null, only list states with one of the given
+     * names.
+     * @param excludes If not null, only list states without one of the given
+     * names.
+     * @return A list of all registered states.
+     */
+    public List<State> listStates(boolean onlyLast, List<String> includes,
+                                  List<String> excludes);
 }
